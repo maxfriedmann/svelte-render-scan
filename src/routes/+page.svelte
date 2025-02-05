8ee7174a574cc9ae9a80167236751ef9e7545153
Stanislav Khromov <stanislav.khromov+github@gmail.com>
2025-02-06 00:43:44 +0100

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -19,7 +19,6 @@
 		const r = parseInt(color.substring(0, 2), 16);
 		const g = parseInt(color.substring(2, 4), 16);
 		const b = parseInt(color.substring(4, 6), 16);
-		console.log(r, g, b);
 		// Calculate perceived brightness using relative luminance
 		return (r * 299 + g * 587 + b * 114) / 1000;
 	}
