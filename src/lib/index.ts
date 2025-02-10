1c405e41492538a3129f3a3fef3b400a70e7368e
Stanislav Khromov <stanislav.khromov+github@gmail.com>
2025-02-10 16:18:34 +0100

    Update index.ts

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,2 +1,3 @@
 // Reexport your entry components here
 export { default as RenderScan } from './components/RenderScan.svelte';
+export { default as RenderScanObserver } from './components/RenderScanObserver.svelte';
